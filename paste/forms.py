5b75f2e4a31338dfe55f6dd5c4ef89f4750acd08
Julien Palard <julien@palard.fr>
2018-05-11 22:56:26 +0200

    FIX: Don't strip initial indentation in content.

--- paste/forms.py
+++ paste/forms.py
@@ -1,11 +1,15 @@
 from webtools import settings
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField
 from .models import Paste, Language, EXPIRE_CHOICES
 
 
 class PasteForm(ModelForm):
     """Paste model form."""
+    content = CharField(max_length=settings.PASTE['max_characters'],
+                        strip=False,
+                        widget=forms.Textarea)
+
     class Meta:
         model = Paste
         fields = ['language', 'title', 'password', 'content', 'lifetime',
