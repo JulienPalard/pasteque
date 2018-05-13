c9e406573f5474be4c704ea93f97014dfb39be8d
Julien Palard <julien@palard.fr>
2018-05-13 12:18:31 +0200

    Add columns in Paste admin.

--- paste/admin.py
+++ paste/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from .models import Paste
 
-admin.site.register(Paste)
+@admin.register(Paste)
+class PasteAdmin(admin.ModelAdmin):
+    list_display = ('paste_time', 'slug', 'title', 'viewcount', 'paste_ip')
