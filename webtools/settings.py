4ff086e268784bb54e0132bb0fde683b063e9459
Julien Palard <julien@palard.fr>
2020-05-31 23:22:23 +0200

    Drop SessionMiddleware (missing imports anyway).

--- webtools/settings.py
+++ webtools/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
 ]
 
 MIDDLEWARE = [
-    "paste.middleware.SessionMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.common.CommonMiddleware",
